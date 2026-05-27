dc770f77d0a8e1c47782afe2854e2684390cc28a
yeraldsanchez <y.sanchez.1@estudiantec.cr>
2026-05-27 11:40:40 -0600

    Add tests for user registration with existing email and short password

--- tests/api/HU01.api.spec.ts
+++ tests/api/HU01.api.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 test.describe('Registro de nuevo usuario', () => {
-  test.only('Registro vía API con nombre de usuario ya en uso', async ({ request }) => {
+  test('Registro vía API con nombre de usuario ya en uso', async ({ request }) => {
     const response = await request.post('/answer/api/v1/user/register/email', {
       data: {
         e_mail: 'nuevo@gmail.com',
@@ -11,4 +11,18 @@ test.describe('Registro de nuevo usuario', () => {
 
     expect(response.status()).toBe(409);
   });
+
+  test('Registro vía API con contraseña demasiado corta', async ({ request }) => {
+    const response = await request.post('/answer/api/v1/user/register/email', {
+      data: {
+        e_mail: 'test2@gmail.com',
+        name: 'usuario',
+        pass: 'abc',
+      },
+    });
+
+    expect(response.status()).toBe(422);
+  });
+
+
 });
--- tests/e2e/HU01.spec.ts
+++ tests/e2e/HU01.spec.ts
@@ -31,4 +31,19 @@ test.describe('Registro de nuevo usuario', () => {
         await expect(page.locator('#root')).toContainText('Correo electrónico debe ser una dirección de correo electrónico válida.');
     });
 
+    test('Registro con correo ya registrado en el sistema', async ({page}) => {
+        await page.goto('/users/register');
+        await page.getByRole('textbox', {name: 'Nombre'}).click();
+        await page.getByRole('textbox', {name: 'Nombre'}).fill('usuario123');
+        await page.getByRole('textbox', {name: 'Correo electrónico'}).click();
+        await page.getByRole('textbox', {name: 'Correo electrónico'}).fill('test@gmail.com');
+        await page.getByRole('textbox', {name: 'Contraseña'}).click();
+        await page.getByRole('textbox', {name: 'Contraseña'}).click();
+        await page.getByRole('textbox', {name: 'Contraseña'}).fill('Segura123');
+        await page.getByRole('button', {name: 'Registrarse'}).click();
+        await expect(page.locator('#root')).toContainText('Correo electrónico ya en uso.');
+
+
+    });
+
 });
